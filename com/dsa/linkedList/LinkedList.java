66465e6131dd50f637312f11e1a7ef6a5f8251e3
Anjon Roy <anjon782446@gmail.com>
2024-01-19 23:36:04 +0530

    linkedList class changes

--- com/dsa/linkedList/LinkedList.java
+++ com/dsa/linkedList/LinkedList.java
@@ -1,4 +1,21 @@
 package com.dsa.linkedList;
 
 public class LinkedList {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String []args){
+        int arr[] = {2,5,6,8};
+        Node node = new Node(arr[0]);
+        System.out.println("Node data "+node.data);
+    }
 }
