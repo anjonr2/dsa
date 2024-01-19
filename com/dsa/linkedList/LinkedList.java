f822845f950ef709bae9532dc1a975e2d8fa5e57
Anjon Roy <anjon782446@gmail.com>
2024-01-19 23:55:30 +0530

    Changes for converting an array to LinkedList

--- com/dsa/linkedList/LinkedList.java
+++ com/dsa/linkedList/LinkedList.java
@@ -1,5 +1,6 @@
 package com.dsa.linkedList;
-
+/*Converting an Array to LinkedList
+* Time complexity is O(n) since it's traversing linearly*/
 public class LinkedList {
     static class Node{
         int data;
@@ -13,9 +14,23 @@ public class LinkedList {
             this.next = null;
         }
     }
+    static Node convertArr2LL(int []arr){
+        /*Since head is always the first node*/
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1; i<arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
     public static void main(String []args){
         int arr[] = {2,5,6,8};
         Node node = new Node(arr[0]);
         System.out.println("Node data "+node.data);
+
+        Node head = convertArr2LL(arr);
+        System.out.println("After converting array to the linkedList "+head.data);
     }
 }
