710899f907e8fd6550bae961d5b631975f04bdc7
Anjon Roy <anjon782446@gmail.com>
2024-01-21 18:11:12 +0530

    insert node at the head of the linkedlist

---
+++ com/dsa/LinkedList2/InsertionLL.java
@@ -0,0 +1,46 @@
+package com.dsa.LinkedList2;
+
+public class InsertionLL {
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data, Node next){
+            this.data=data;
+            this.next=next;
+        }
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    static Node convertarr2LL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node temp = head;
+        for(int i=1; i<arr.length; i++){
+            Node node = new Node(arr[i]);
+            temp.next = node;
+            temp = temp.next;
+        }
+        return head;
+    }
+    static void traverse(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+    static Node insertAtHead(Node head, int value){
+        Node temp = new Node(value,head);
+        return temp;
+    }
+    public static void main(String []args){
+        int arr[] = {12,5,8,7};
+        Node head = convertarr2LL(arr);
+        System.out.println("After converting the array to LinkedList");
+        traverse(head);
+        head = insertAtHead(head,6);
+        System.out.println("After insertion linkedlist");
+        traverse(head);
+    }
+}
