d20c79cdd93428d83e05bb9002f0783fdda2e2ab
Anjon Roy <anjon782446@gmail.com>
2024-01-31 01:35:14 +0530

    brute force solution to sort list of 0's,1's and 2's

--- com/dsa/linkedlistset2/LinkedList2.java
+++ com/dsa/linkedlistset2/LinkedList2.java
@@ -40,13 +40,58 @@ public class LinkedList2 {
         odd.next=evenHead;
         return head;
     }
+    /*Sort a list of 0's,1's,2's*/
+    public static Node sort(Node head){
+        Node temp = head;
+        /*initialized three variables to keep a count of 0's,1's and 2's present in the list*/
+        int cnt0=0,cnt1=0,cnt2=0;
+        /*After this traversal cnt0,1 and 2 will have the total count of 0's, 1's amd 2's present in the list*/
+        while (temp != null){
+            switch (temp.data){
+                case 0 :{
+                    cnt0++;
+                    break;
+                }
+                case 1 :{
+                    cnt1++;
+                    break;
+                }
+                case 2 :{
+                    cnt2++;
+                    break;
+                }
+            }
+            temp=temp.next;
+        }
+        temp = head;
+        while (temp!=null){
+            if(cnt0>0){
+                temp.data=0;
+                cnt0--;
+            } else if (cnt1>0) {
+                temp.data=1;
+                cnt1--;
+            }else {
+                temp.data=2;
+                cnt2--;
+            }
+            temp=temp.next;
+        }
+        return  head;
+    }
     public static void main(String []args){
         int arr[] = {2, 5, 6, 8, 4};
+        int arr1[] = {1, 1, 0, 0, 2,1,2,0};
         Node head = convertarr2LL(arr);
         System.out.println("After converting array to list");
         print(head);
         head = groupOddEven(head);
+        Node head1 = convertarr2LL(arr1);
         System.out.println("After grouping odd and even nodes together");
         print(head);
+
+        Node head2 = sort(head1);
+        System.out.println("After sorting the list of 0's, 1's & 2's");
+        print(head2);
     }
 }
