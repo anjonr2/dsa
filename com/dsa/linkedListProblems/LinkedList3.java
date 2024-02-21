24e65dee80c401d6a5d463830f77d76e584dc8e5
Anjon Roy <anjon782446@gmail.com>
2024-02-21 23:40:30 +0530

    code to add 1 to a linkedlist

--- com/dsa/linkedListProblems/LinkedList3.java
+++ com/dsa/linkedListProblems/LinkedList3.java
@@ -40,14 +40,74 @@ public class LinkedList3 {
         }
         return back;
     }
+    public static Node findMiddle(Node head){
+        Node slow=head, fast = head;
+        while (fast.next.next != null && fast.next != null){
+            //moves one node at a time
+            slow = slow.next;
+
+            //moves two Node at a tine
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static boolean checkPalindrome(Node head){
+        Node middle = findMiddle(head);
+        Node newHead = reverse(middle.next);
+        Node first = head;
+        Node second = newHead;
+        while (second!=null){
+            if(first.data!=second.data){
+                reverse(newHead);
+                return false;
+            }
+            /*Move first and second by one node*/
+            first = first.next;
+            second = second.next;
+        }
+        reverse(newHead);
+        return true;
+    }
+    public static int addHelper(Node node){
+        if(node == null){
+            return 1;
+        }
+        int carry = addHelper(node.next);
+        node.data=node.data+carry;
+        if(node.data<10){
+            return 0;
+        }
+        node.data=0;
+        return 1;
+    }
+    public static Node addOne(Node head){
+        int carry = addHelper(head);
+        if(carry==1){
+            Node node = new Node(1);
+            node.next=head;
+            head=node;
+        }
+        return head;
+    }
     public static void main(String []args){
         int arr[] = {2, 5, 6, 8, 4};
+        int arr1[] = {9, 9, 9, 9, 9};
         Node head = convertArr2LL(arr);
+        Node head2 = convertArr2LL(arr1);
         System.out.println("After converting array to LinkedList");
         traverse(head);
 
-        head = reverse(head);
-        System.out.println("After reversing the linkedList");
-        traverse(head);
+//        head = reverse(head);
+//        System.out.println("After reversing the linkedList");
+//        traverse(head);
+
+//        Boolean palindrome = checkPalindrome(head);
+//        System.out.println("Given linkedList is palindrome "+palindrome);
+
+         Node newHead = addOne(head);
+         head2 = addOne(head2);
+         System.out.println("After adding 1 to the linkedList");
+         traverse(newHead);
+         traverse(head2);
     }
 }
