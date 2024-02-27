d221295c0cf7e465e6f3b8314f388451000cba61
Anjon Roy <anjon782446@gmail.com>
2024-02-27 23:44:50 +0530

    [recursion : initial commit]

---
+++ com/dsa/recursion/BasicRecursion.java
@@ -0,0 +1,15 @@
+package com.dsa.recursion;
+
+import java.util.Scanner;
+
+public class BasicRecursion {
+    public static void printName(int i,int n){
+        if(i>n)
+            return;
+    }
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of time");
+        int count = sc.nextInt();
+    }
+}
