f8d09a0a34353804ebd4deba6d39aefa8add9456
Anjon Roy <anjon782446@gmail.com>
2024-02-28 00:20:58 +0530

    printing Number N to 1 using recursion

--- com/dsa/recursion/BasicRecursion.java
+++ com/dsa/recursion/BasicRecursion.java
@@ -11,11 +11,21 @@ public class BasicRecursion {
         System.out.println("test");
         printName(++i,n);
     }
+    public static void printNto1(int i, int N){
+        if(N<1){
+            return;
+        }
+        System.out.println(N);
+        printNto1(i,--N);
+    }
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no of time");
         int count = sc.nextInt();
-        System.out.println("printing name "+count+" times");
-        printName(1,count);
+//        System.out.println("printing name "+count+" times");
+//        printName(1,count);
+
+        System.out.println("printing number N to 1");
+        printNto1(1,count);
     }
 }
