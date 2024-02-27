6f8b377daa9b5788a2208a9973be8e6a204d388c
Anjon Roy <anjon782446@gmail.com>
2024-02-28 00:43:22 +0530

    sum of first n numbers using recursion

--- com/dsa/recursion/BasicRecursion.java
+++ com/dsa/recursion/BasicRecursion.java
@@ -18,6 +18,22 @@ public class BasicRecursion {
         System.out.println(N);
         printNto1(i,--N);
     }
+    /*print number from 1 to n using backtracking*/
+    public static void printNumbers(int i,int n){
+        if(i<1){
+            return;
+        }
+        printNumbers(--i,n);
+        System.out.println(i);
+    }
+    /*Print summation of first i numbers using recursion*/
+    public static void printSum(int i, int sum){
+        if(i<1){
+            System.out.println(sum);
+            return;
+        }
+        printSum(i-1,sum+i);
+    }
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no of time");
@@ -25,7 +41,8 @@ public class BasicRecursion {
 //        System.out.println("printing name "+count+" times");
 //        printName(1,count);
 
-        System.out.println("printing number N to 1");
-        printNto1(1,count);
+        System.out.println("printing sum");
+//        printNto1(1,count);
+        printSum(count,0);
     }
 }
