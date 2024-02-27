6cdb777e63579e8e400db5966ffdc1a9c1b07831
Anjon Roy <anjon782446@gmail.com>
2024-02-28 00:11:39 +0530

    pre-incremented count i to avoid stackoverflow

--- com/dsa/recursion/BasicRecursion.java
+++ com/dsa/recursion/BasicRecursion.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class BasicRecursion {
     /*recursively printing name from 1 to N*/
     public static void printName(int i,int n){
-        if(i>n) return;
+        if(i>n){
+            return;
+        }
         System.out.println("test");
-        printName(i++,n);
+        printName(++i,n);
     }
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no of time");
         int count = sc.nextInt();
-        System.out.println("printing name "+count+"times");
-        printName(1,5);
+        System.out.println("printing name "+count+" times");
+        printName(1,count);
     }
 }
