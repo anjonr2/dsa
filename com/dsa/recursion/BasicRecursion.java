eb07b51fa693108a1fd34edad844a410ba813430
Anjon Roy <anjon782446@gmail.com>
2024-02-27 23:48:15 +0530

    recursively printing name

--- com/dsa/recursion/BasicRecursion.java
+++ com/dsa/recursion/BasicRecursion.java
@@ -3,13 +3,17 @@ package com.dsa.recursion;
 import java.util.Scanner;
 
 public class BasicRecursion {
+    /*recursively printing name from 1 to N*/
     public static void printName(int i,int n){
-        if(i>n)
-            return;
+        if(i>n) return;
+        System.out.println("test");
+        printName(i++,n);
     }
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no of time");
         int count = sc.nextInt();
+        System.out.println("printing name "+count+"times");
+        printName(1,5);
     }
 }
