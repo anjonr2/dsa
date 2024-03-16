48056c0eb3ddbffede234bfd63a7d7c6450b0bda
Anjon Roy <anjon782446@gmail.com>
2024-03-16 13:46:04 +0530

    sub seq with sum k

--- com/dsa/recursion/SubSequenceWithSumK.java
+++ com/dsa/recursion/SubSequenceWithSumK.java
@@ -4,9 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SubSequenceWithSumK {
-    private static List<List<Integer>> subseqWithSumK(){
+    private static List<List<Integer>> subseqWithSumK(int[] input, int sum){
         List<List<Integer>> result = new ArrayList<>();
+        findSubSeq(input,sum,0,new ArrayList<>(),result);
         return result;
+    }
+    private static void findSubSeq(int[] input,int sum, int idx,List<Integer> current,List<List<Integer>> result){
+        if(idx==input.length){
+            if(sum(current)==sum){
+                result.add(new ArrayList<>(current));
+            }
+            return;
+        }
+        //pick the element at current idx
+        current.add(idx);
+        findSubSeq(input,sum,idx+1,current,result);
+
+        //backtrack and remove the current element
+        current.remove(current.size()-1);
+        findSubSeq(input,sum,idx+1,current,result);
     }
     private static int sum(List<Integer> list){
         int total = 0;
@@ -16,6 +32,13 @@ public class SubSequenceWithSumK {
         return total;
     }
     public static void main(String[] args){
-        System.out.println("Test");
+        int [] nums = {1, 2, 3, 4, 5};
+        int sum = 5;
+        List<List<Integer>> subsequences = subseqWithSumK(nums,sum);
+
+        System.out.println("Subsequences with sum "+sum+" :");
+        for(List<Integer> subseq:subsequences){
+            System.out.println(subseq);
+        }
     }
 }
