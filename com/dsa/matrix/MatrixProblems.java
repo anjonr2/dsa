15e8b01b75ecd1218200e3e34eac5d0c073defc6
Anjon Roy <anjon782446@gmail.com>
2024-04-12 14:39:43 +0530

    spiral traversal

--- com/dsa/matrix/MatrixProblems.java
+++ com/dsa/matrix/MatrixProblems.java
@@ -10,31 +10,38 @@ public class MatrixProblems {
         int left = 0, right = cols - 1;
         int top = 0, bottom = rows - 1;
         List<Integer> result = new ArrayList<>();
-        // traverse right first
-        for (int i = left; i <= right; i++) {
-            result.add(matrix.get(top).get(i));
-        }
-        // top will move to next row
-        top++;
 
-        // traverse top to bottom where col(right) will be const
-        for (int i = top; i <= bottom; i++) {
-            result.add(matrix.get(i).get(right));
-        }
-        right--;
+        while (top <= bottom && left <= right) {
+            // traverse right first
+            for (int i = left; i <= right; i++) {
+                result.add(matrix.get(top).get(i));
+            }
+            // top will move to next row
+            top++;
 
-        for (int i = right; i >= left; i--) {
-            // row will be const so bottom will be same
-            result.add(matrix.get(bottom).get(i));
-        }
-        bottom--;
+            // traverse top to bottom where col(right) will be const
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix.get(i).get(right));
+            }
+            right--;
 
-        // traverse from bottom to top only row will be changing
-        for (int i = bottom; i >= top; i--) {
-            result.add(matrix.get(i).get(left));
+            if (top <= bottom) {
+
+                for (int i = right; i >= left; i--) {
+                    // row will be const so bottom will be same
+                    result.add(matrix.get(bottom).get(i));
+                }
+                bottom--;
+            }
+            // traverse from bottom to top only row will be changing
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix.get(i).get(left));
+                }
+                // column will be incremented to next column
+                left++;
+            }
         }
-        // column will be incremented to next column
-        left++;
         return result;
     }
 
