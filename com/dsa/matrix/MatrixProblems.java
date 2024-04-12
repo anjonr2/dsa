fc068ab0a465a74b4ccde081d1dafcee6f70b5bd
Anjon Roy <anjon782446@gmail.com>
2024-04-12 14:19:31 +0530

    spiral traversal

---
+++ com/dsa/matrix/MatrixProblems.java
@@ -0,0 +1,16 @@
+package com.dsa.matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatrixProblems {
+    public static List<Integer> spiralMatrix(List<List<Integer>> matrix) {
+        int rows = matrix.size();
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
