bda87f745df3f00e8f41bcd5eb34840c656e1e1d
Anjon Roy <anjon782446@gmail.com>
2024-05-04 21:40:40 +0530

    deleting a node in a BST

--- com/dsa/tree/BinarySearchTree.java
+++ com/dsa/tree/BinarySearchTree.java
@@ -74,4 +74,50 @@ public class BinarySearchTree {
         }
         return root;
     }
+
+    public Node deleteNode(Node root, int value) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data == value) {
+            return helper(root);
+        }
+        Node temp = root;
+        while (root != null) {
+            if (root.data > value) {
+                if (root.left != null && root.left.data == value) {
+                    root.left = helper(root.left);
+                    break;
+                } else {
+                    root = root.left;
+                }
+            } else {
+                if (root.right != null && root.right.data == value) {
+                    root.right = helper(root.right);
+                    break;
+                } else {
+                    root = root.right;
+                }
+            }
+        }
+        return temp;
+    }
+
+    public Node helper(Node root) {
+        if (root.left == null) {
+            return root.right;
+        } else if (root.right == null) {
+            return root.left;
+        }
+        Node rightChild = root.right;
+        Node lastRight = findLastRight(root.left);
+        lastRight.right = rightChild;
+        return root.left;
+    }
+
+    public Node findLastRight(Node root) {
+        if (root.right == null)
+            return root;
+        return findLastRight(root.right);
+    }
 }
