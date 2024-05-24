1b934d58ad1906ecbe1380518f8e5a76f1b786f6
Anjon Roy <anjon782446@gmail.com>
2024-05-24 10:00:11 +0530

    remove kth node from end of the list

--- com/dsa/linkedList/AddToLinkedList.java
+++ com/dsa/linkedList/AddToLinkedList.java
@@ -61,4 +61,54 @@ public class AddToLinkedList {
         // O(n)
         return reverse(head);
     }
+
+    public static Node findNthFromLast(Node head, int k) {
+        int length = 1;
+        Node temp = head;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        int kthNode = length - k + 1;
+        temp = head;
+        while (kthNode > 0 && temp != null) {
+            kthNode--;
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public static Node removeKthNode(Node head, int K) {
+        if (head == null) {
+            return null;
+        }
+
+        int length = 0;
+        Node temp = head;
+        while (temp != null) {
+            // using this loop calculate length of the loop
+            length++;
+            temp = temp.next;
+        }
+
+        if (length == K) {
+            // head node has been asked to remove
+            return head.next;
+        }
+
+        int res = length - K;
+        temp = head;
+        while (temp != null) {
+            res--;
+            if (res == 0) {
+                break;
+            }
+            temp = temp.next;
+        }
+        Node delNode = temp.next;
+        temp.next = delNode.next;
+        delNode.next = null;
+
+        return head;
+    }
 }
