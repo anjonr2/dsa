306dc4f1dd63a9757d4ac087f4dd6db2a0b7a93b
Anjon Roy <anjon782446@gmail.com>
2024-06-13 12:07:01 +0530

    find floor and ceil

---
+++ com/dsa/array/FloorAndCeil.java
@@ -0,0 +1,43 @@
+package com.dsa.array;
+
+public class FloorAndCeil {
+    static int findFloor(int[] arr, int n, int x) {
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        int floor = findFloor(arr, n, x);
+        int ceil = findCeil(arr, n, x);
+        return new int[] { floor, ceil };
+    }
+}
