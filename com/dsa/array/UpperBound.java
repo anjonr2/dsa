d3b4fedeb887c3236e57d0d840964d1f351b6d27
Anjon Roy <anjon782446@gmail.com>
2024-06-13 11:03:54 +0530

    upper bound

---
+++ com/dsa/array/UpperBound.java
@@ -0,0 +1,19 @@
+package com.dsa.array;
+
+public class UpperBound {
+    public static int upperBound(int[] arr, int n, int x) {
+        int ans = n;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (arr[middle] > x) {
+                ans = middle;
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return ans;
+    }
+}
