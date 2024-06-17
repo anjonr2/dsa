542ef68a4ccbe6b00949c5ce2b9098c7b19ff2c0
Anjon Roy <anjon782446@gmail.com>
2024-06-17 13:30:43 +0530

    optimal: binary search approach to find square root

--- com/dsa/array/SquareRoot.java
+++ com/dsa/array/SquareRoot.java
@@ -13,4 +13,23 @@ public class SquareRoot {
         }
         return ans;
     }
+
+    public static int sqrtN(long N) {
+        if (N == 0 || N == 1) {
+            return (int) N;
+        }
+        long low = 1;
+        long high = N;
+        int ans = 1;
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (mid * mid <= N) {
+                ans = (int) mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
 }
