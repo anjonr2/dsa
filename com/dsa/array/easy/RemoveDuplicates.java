98cae456e1d5e2c5aad179dbe680151f98c3eea4
Anjon Roy <anjon782446@gmail.com>
2024-06-23 19:30:55 +0530

    optimal : remove duplicates

--- com/dsa/array/easy/RemoveDuplicates.java
+++ com/dsa/array/easy/RemoveDuplicates.java
@@ -17,4 +17,19 @@ public class RemoveDuplicates {
         }
         return k;
     }
+
+    static int removeDuplicates1(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
 }
