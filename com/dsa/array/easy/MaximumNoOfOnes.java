bb4f068157e3c5fa37426f4bc73231f039eea968
Anjon Roy <anjon782446@gmail.com>
2024-06-27 13:42:46 +0530

    optimal approach to find row with maximum no of one

--- com/dsa/array/easy/MaximumNoOfOnes.java
+++ com/dsa/array/easy/MaximumNoOfOnes.java
@@ -18,4 +18,37 @@ public class MaximumNoOfOnes {
         }
         return index;
     }
+
+    public int lowerBound(int[] arr, int n, int x) {
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int rowAndMaximumOnes1(int[][] mat) {
+        int cntMax = -1;
+        int index = -1;
+        int n = mat.length;
+        int m = mat[0].length;
+
+        for (int i = 0; i < n; i++) {
+            int cntOfOnes = m - lowerBound(mat[i], m, 1);
+            if (cntOfOnes > cntMax) {
+                cntMax = cntOfOnes;
+                index = i;
+            }
+        }
+        return index;
+    }
 }
