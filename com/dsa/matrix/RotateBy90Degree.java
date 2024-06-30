a6cea91d7115644b889cdb486fd2bff73a7894b0
Anjon Roy <anjon782446@gmail.com>
2024-06-30 21:32:37 +0530

    Optimal approach: rotate matrix by 90 degree

--- com/dsa/matrix/RotateBy90Degree.java
+++ com/dsa/matrix/RotateBy90Degree.java
@@ -16,4 +16,35 @@ public class RotateBy90Degree {
             }
         }
     }
+
+    public void rotate1(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix, i, j);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            // reverse every row
+            reverse(matrix[i]);
+        }
+    }
+
+    public void swap(int[][] matrix, int i, int j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+    }
+
+    public void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
