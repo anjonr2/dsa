5bd480fbf1c466c4c4d6d265c325125449c31f98
Anjon Roy <anjon782446@gmail.com>
2024-07-20 14:16:54 +0530

    better solution to find longest subseq

--- com/dsa/array/medium/LongestConsecutiveSubSequence.java
+++ com/dsa/array/medium/LongestConsecutiveSubSequence.java
@@ -1,5 +1,7 @@
 package com.dsa.array.medium;
 
+import java.util.Arrays;
+
 public class LongestConsecutiveSubSequence {
     public boolean linearSearch(int[] nums, int el) {
         for (int i = 0; i < nums.length; i++) {
@@ -18,6 +20,24 @@ public class LongestConsecutiveSubSequence {
             while (linearSearch(nums, x + 1) == true) {
                 x += 1;
                 cnt += 1;
+            }
+            longest = Math.max(longest, cnt);
+        }
+        return longest;
+    }
+
+    public static int lengthOfLongestConsecutiveSequence(int[] arr, int N) {
+        Arrays.sort(arr);
+        int longest = 1;
+        int cnt = 0;
+        int lastSmallest = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] - 1 == lastSmallest) {
+                cnt++;
+                lastSmallest = arr[i];
+            } else if (arr[i] != lastSmallest) {
+                cnt = 1;
+                lastSmallest = arr[i];
             }
             longest = Math.max(longest, cnt);
         }
