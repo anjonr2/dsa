4fd17044c62e839a6c8b77c1ffb4040eec1cff9b
Anjon Roy <anjon782446@gmail.com>
2024-07-21 12:36:06 +0530

    optimal solution to find Sub Array with maximum sum

--- com/dsa/array/medium/MaximumSumSubArray.java
+++ com/dsa/array/medium/MaximumSumSubArray.java
@@ -29,4 +29,29 @@ public class MaximumSumSubArray {
             maximum = 0;
         return maximum;
     }
+
+    public static int[] maxSumSubarray(int[] arr, int n) {
+        int maximum = Integer.MIN_VALUE;
+        int sum = 0;
+        int[] ans = new int[2];
+        int start = -1;
+        int startIdx = -1;
+        int endIdx = -1;
+        for (int i = 0; i < n; i++) {
+            if (sum == 0)
+                start = i;
+            sum += arr[i];
+            if (sum > maximum) {
+                maximum = sum;
+                startIdx = start;
+                endIdx = i;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        if (maximum < 0)
+            maximum = 0;
+        return new int[] { startIdx, endIdx };
+    }
 }
