a599106d9f90e2d2c2534aeb82932b48efaf0c15
Anjon Roy <anjon782446@gmail.com>
2024-08-03 11:48:55 +0530

    convert arr to Linkedlist

---
+++ com/dsa/linkedList/utils/LinkedList.java
@@ -0,0 +1,35 @@
+package com.dsa.linkedList.utils;
+
+public class LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static Node convertArr2LL(int arr[]) {
+        Node head = new Node(arr[0]);
+        Node temp = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node node = new Node(arr[i]);
+            temp.next = node;
+            temp = node;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 5, 6, 8 };
+        Node head = convertArr2LL(arr);
+        System.out.println("head data " + head.data);
+    }
+}
