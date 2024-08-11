66c08bf5e8904a9e6dc220a5a11c94765ed1e27a
Anjon Roy <anjon782446@gmail.com>
2024-08-11 16:07:21 +0530

    logic to remove kth element from  a  DLL

--- com/dsa/linkedList/utils/DoublyLinkedList.java
+++ com/dsa/linkedList/utils/DoublyLinkedList.java
@@ -62,6 +62,37 @@ public class DoublyLinkedList {
         return head;
     }
 
+    public static Node removeKthElement(Node head, int k) {
+        if (head == null) {
+            return null;
+        }
+        Node temp = head;
+        int cnt = 0;
+        while (temp != null) {
+            cnt++;
+            if (cnt == k) {
+                break;
+            }
+            temp = temp.next;
+        }
+        Node back = temp.prev;
+        Node front = temp.next;
+
+        if (back == null && front == null) {
+            return null;
+        } else if (back == null) {
+            deleteHead(temp);
+        } else if (front == null) {
+            deleteTail(temp);
+        } else {
+            back.next = temp.next;
+            front.prev = temp.prev;
+            temp.prev = null;
+            temp.next = null;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 12, 5, 6, 8, 9, 20, 56 };
         Node node = arr2DLLNode(arr);
@@ -70,5 +101,9 @@ public class DoublyLinkedList {
         node = deleteTail(node);
         System.out.println("Doubly linked list after deleting tail---");
         traverse(node);
+
+        Node head = removeKthElement(node, 2);
+        System.out.println("Linked List after deleting 2nd element");
+        traverse(head);
     }
 }
