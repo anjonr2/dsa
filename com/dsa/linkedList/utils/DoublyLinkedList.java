e95fb34cbbfd9f681f8e39361ec97410aa27b70b
Anjon Roy <anjon782446@gmail.com>
2024-08-11 13:06:39 +0530

    array to doubly linked list

---
+++ com/dsa/linkedList/utils/DoublyLinkedList.java
@@ -0,0 +1,38 @@
+package com.dsa.linkedList.utils;
+
+public class DoublyLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data, Node next, Node prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node arr2DLLNode(int arr[]) {
+        Node head = new Node(arr[0]);
+        Node prev = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node node = new Node(arr[i], null, prev);
+            prev.next = node;
+            prev = node;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 12, 5, 6, 8, 9, 20, 56 };
+        Node node = arr2DLLNode(arr);
+        System.out.println(node.data);
+    }
+}
