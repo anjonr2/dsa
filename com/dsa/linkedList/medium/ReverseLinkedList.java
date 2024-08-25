180de312f181e88be71a813cbf7f7a16ca1bd652
Anjon Roy <anjon782446@gmail.com>
2024-08-25 13:23:18 +0530

    approach3:recurisvely reverse a linkedlist

--- com/dsa/linkedList/medium/ReverseLinkedList.java
+++ com/dsa/linkedList/medium/ReverseLinkedList.java
@@ -54,4 +54,15 @@ public class ReverseLinkedList {
         }
         return prev;
     }
+
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList2(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
 }
