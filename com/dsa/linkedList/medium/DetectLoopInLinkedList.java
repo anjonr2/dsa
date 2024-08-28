3efe89cff82bb4b9a2401c66b6b19531ec3c9019
Anjon Roy <anjon782446@gmail.com>
2024-08-28 10:39:27 +0530

    detect loop in a linkedlist

---
+++ com/dsa/linkedList/medium/DetectLoopInLinkedList.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class DetectLoopInLinkedList {
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public ListNode(int data, ListNode next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode temp = head;
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        while (temp != null) {
+            if (map.containsKey(temp)) {
+                return true;
+            }
+            map.put(temp, 1);
+            temp = temp.next;
+        }
+        return false;
+    }
+}
