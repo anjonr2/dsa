ddea38bbf4753298d463a24389fef82f2c933fe9
Anjon Roy <anjon782446@gmail.com>
2024-09-01 16:50:01 +0530

    optimal approach to check if a linked list is palindrome

--- com/dsa/linkedList/medium/PalindromeList.java
+++ com/dsa/linkedList/medium/PalindromeList.java
@@ -20,4 +20,41 @@ public class PalindromeList {
         }
         return true;
     }
+
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverse(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static boolean isPalindrome1(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // find middle of the linked list
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode newHead = reverse(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+        while (second != null) {
+            if (first.data != second.data) {
+                reverse(newHead);
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        reverse(newHead);
+        return true;
+    }
 }
