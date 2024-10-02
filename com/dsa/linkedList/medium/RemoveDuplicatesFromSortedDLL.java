c4d2f69d5dec9084dd8d52f3b4177d2d4aa607fb
Anjon Roy <anjon782446@gmail.com>
2024-10-02 20:14:36 +0530

    Remove duplicates from sorted DLL

---
+++ com/dsa/linkedList/medium/RemoveDuplicatesFromSortedDLL.java
@@ -0,0 +1,36 @@
+package com.dsa.linkedList.medium;
+
+public class RemoveDuplicatesFromSortedDLL {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+
+        public Node(int data, Node next, Node prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
+
+    public static Node removeDuplicates(Node head) {
+        Node temp = head;
+        while (temp != null && temp.next != null) {
+            Node nextNode = temp.next;
+            while (temp.data == nextNode.data) {
+                nextNode = nextNode.next;
+            }
+            temp.next = nextNode;
+            if (nextNode != null)
+                nextNode.prev = temp;
+        }
+        return head;
+    }
+}
