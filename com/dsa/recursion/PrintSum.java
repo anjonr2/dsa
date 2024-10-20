6845ff8159a130fc7ab5c46b5c723019f2e92ec6
Anjon Roy <anjon782446@gmail.com>
2024-10-20 18:12:49 +0530

    sum of first n numbers

---
+++ com/dsa/recursion/PrintSum.java
@@ -0,0 +1,22 @@
+package com.dsa.recursion;
+
+public class PrintSum {
+    // public static void printSum(int i, int sum) {
+    // if (i < 1) {
+    // System.out.println("Sum is " + sum);
+    // }
+    // printSum(i - 1, sum + i);
+    // }
+    public static int printSum(int n) {
+        if (n == 0) {
+            // sum of first 0 numbers is 0
+            return 0;
+        }
+        return n + printSum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int sum = printSum(3);
+        System.out.println("sum " + sum);
+    }
+}
