6f97589ffa026d4b92f85dc9fcb304f330c0a0f1
Anjon Roy <anjon782446@gmail.com>
2024-10-20 18:17:04 +0530

    factorial of first n numbers

--- com/dsa/recursion/PrintSum.java
+++ com/dsa/recursion/PrintSum.java
@@ -15,8 +15,17 @@ public class PrintSum {
         return n + printSum(n - 1);
     }
 
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
     public static void main(String[] args) {
         int sum = printSum(3);
         System.out.println("sum " + sum);
+        int factorial = factorial(4);
+        System.out.println("factorial of 4 is " + factorial);
     }
 }
