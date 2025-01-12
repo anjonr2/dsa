660a63fd13c2c96488e808261552df93f29fbc11
Anjon Roy <anjon782446@gmail.com>
2025-01-12 20:49:22 +0530

    Array impl of queue

---
+++ com/dsa/queue/QueueArrImpl.java
@@ -0,0 +1,49 @@
+package com.dsa.queue;
+
+public class QueueArrImpl implements Queue {
+    private int currSize = 0;
+    private int front = -1, rear = -1;
+    private int capacity = 10;
+    int queue[] = new int[capacity];
+
+    @Override
+    public void enqueue(int x) {
+        if (currSize == capacity) {
+            return;
+        }
+        if (rear == -1) {
+            front = 0;
+        }
+        rear = (rear + 1) % capacity;
+        queue[rear] = x;
+        currSize += 1;
+    }
+
+    @Override
+    public int dequeue() {
+        if (currSize == 0) {
+            return -1;
+        }
+        int el = queue[front];
+        if (currSize == 1) {
+            // checks if it's last element
+            // then reset the queue
+            front = -1;
+            rear = -1;
+        }
+        front = (front + 1) % capacity;
+        currSize -= 1;
+        return el;
+    }
+
+    @Override
+    public int front() {
+        return queue[front];
+    }
+
+    @Override
+    public int size() {
+        return currSize;
+    }
+
+}
