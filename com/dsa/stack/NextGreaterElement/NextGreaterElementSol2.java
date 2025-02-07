6f4930d312008d182fe39580cbbc099a6bae156d
Anjon Roy <anjon782446@gmail.com>
2025-02-07 12:59:50 +0530

    optimal solution

---
+++ com/dsa/stack/NextGreaterElement/NextGreaterElementSol2.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class NextGreaterElementSol2 {
+    public static int[] nextGreaterElement(int[] arr, int n) {
+        int ans[] = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return ans;
+    }
+}
