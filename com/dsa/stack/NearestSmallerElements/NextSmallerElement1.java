88b835fb805e5b7184994e8742257ebf03a6bf1b
Anjon Roy <anjon782446@gmail.com>
2025-02-15 23:09:16 +0530

    comments added

--- com/dsa/stack/NearestSmallerElements/NextSmallerElement1.java
+++ com/dsa/stack/NearestSmallerElements/NextSmallerElement1.java
@@ -1,5 +1,11 @@
 package com.dsa.stack.NearestSmallerElements;
 
+/*input: {4,10,5,8,20,15,3,12}
+there is no smaller element on left of 4 so we put -1
+next 4 is smaller than 10
+next 4 is smaller than 5
+ * output: {-1,4,4,5,8,8,-1,3}
+ */
 import java.util.Stack;
 
 public class NextSmallerElement1 {
