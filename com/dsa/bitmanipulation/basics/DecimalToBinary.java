fe4cfcd2b7ffde90e1180de9e76b9312168c44aa
Anjon Roy <anjon782446@gmail.com>
2025-06-21 15:34:59 +0530

    Decimal to Binary conversion

---
+++ com/dsa/bitmanipulation/basics/DecimalToBinary.java
@@ -0,0 +1,15 @@
+package com.dsa.bitmanipulation.basics;
+
+public class DecimalToBinary {
+    public String decimalToBinary(int n) {
+        StringBuilder binary = new StringBuilder();
+        while (n != 1) {
+            if (n % 2 == 1)
+                binary.append("1");
+            else
+                binary.append("0");
+            n = n / 2;
+        }
+        return binary.toString();
+    }
+}
