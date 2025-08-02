75ed1da41f95f270b4fc24e1ae1666b6ff8759f1
Anjon Roy <anjon782446@gmail.com>
2025-08-02 13:27:49 +0530

    Lowest common ancestor

---
+++ com/dsa/BinaryTree/hard/LowestCommonAncestor.java
@@ -0,0 +1,21 @@
+package com.dsa.BinaryTree.hard;
+
+import com.dsa.BinaryTree.util.Node;
+
+public class LowestCommonAncestor {
+    public Node lowestCommonAncestor(Node root, Node p, Node q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        Node left = lowestCommonAncestor(root.left, p, q);
+        Node right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+}
