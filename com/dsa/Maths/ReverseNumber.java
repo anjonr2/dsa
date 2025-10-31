f90c610cfbb645a889c49742cacd06dac57e9aed
Anjon Roy <anjon782446@gmail.com>
2025-10-31 11:06:06 +0530

    reverse a number

---
+++ com/dsa/Maths/ReverseNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static int reverseNumber(int number) {
+
+        int reverseNumber = 0;
+        while (number > 0) {
+            int lastDigit = number % 10;
+            reverseNumber = reverseNumber * 10 + lastDigit;
+            number = number / 10;
+        }
+        return reverseNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to be reversed");
+        int number = sc.nextInt();
+        System.out.println("Number entered " + number);
+        int reversedNumber = reverseNumber(number);
+        System.out.println("Reversed number is " + reversedNumber);
+        sc.close();
+    }
+}
