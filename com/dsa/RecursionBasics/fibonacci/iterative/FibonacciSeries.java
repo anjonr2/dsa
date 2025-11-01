779d9b249132b5d54a3d120410c5f268bc335139
Anjon Roy <anjon782446@gmail.com>
2025-11-01 12:14:10 +0530

    fibonacci series iterative solution

---
+++ com/dsa/RecursionBasics/fibonacci/iterative/FibonacciSeries.java
@@ -0,0 +1,13 @@
+package com.dsa.RecursionBasics.fibonacci.iterative;
+
+public class FibonacciSeries {
+    public int[] fibonacci(int n) {
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i += 1) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib;
+    }
+}
