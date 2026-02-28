268e8a38e2e8756021d70a4c20b2067355d721c8
Anjon Roy <anjon782446@gmail.com>
2026-02-28 22:20:08 +0530

    optimal approach : four sum

--- com/problemsolving/array/hard/foursum/optimal/foursum.java
+++ com/problemsolving/array/hard/foursum/optimal/foursum.java
@@ -15,8 +15,36 @@ public class foursum {
                 continue; // this check ensures that we are not using the same element for the first
                           // position of the quadruplet
             for (int j = i + 1; j < n; j += 1) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
+                if (j > i + 1 && nums[j] == nums[j - 1]) // check to skip duplicates for the secondposition of the
+                                                         // quadruplet
                     continue;
+
+                // two pointer approach
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        // found the quadruplet
+                        List<Integer> quadruplet = Arrays.asList(nums[i], nums[j], nums[left], nums[right]);
+                        result.add(quadruplet);
+
+                        left += 1;
+                        right -= 1;
+
+                        while (left < right && nums[left] == nums[left - 1])
+                            left += 1;
+                        while (left < right && nums[right] == nums[right + 1])
+                            right -= 1;
+                    } else if (sum < target) {
+                        left += 1;
+                    } else {
+                        right -= 1;
+                    }
+
+                }
+
             }
 
         }
